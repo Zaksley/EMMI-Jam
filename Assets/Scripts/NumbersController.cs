247020c5b88fe6eac905aab00f6a02ec55d0a56f
Zaksley <enzo8550@gmail.com>
2022-04-08 21:42:50 +0200

    Push safe first draft

---
+++ Assets/Scripts/NumbersController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class NumbersController : MonoBehaviour
+{
+
+    public GameObject Number1; 
+    public GameObject Number2; 
+    public GameObject Number3; 
+    public GameObject Number4; 
+
+    private TMP_Text Text1; 
+    private TMP_Text Text2;
+    private TMP_Text Text3; 
+    private TMP_Text Text4; 
+
+    private List<TMP_Text> Texts; 
+
+    private int turn; 
+    [SerializeField] private int maxNumbers = 4; 
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        turn = 0; 
+        Text1 = Number1.GetComponentInChildren<TextMeshPro>(); 
+        Text2 = Number2.GetComponentInChildren<TextMeshPro>(); 
+        Text3 = Number3.GetComponentInChildren<TextMeshPro>(); 
+        Text4 = Number4.GetComponentInChildren<TextMeshPro>(); 
+
+        Texts = new List<TMP_Text>() {Text1, Text2, Text3, Text4}; 
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+            UpdateText(3); 
+    }
+
+
+    void UpdateText(int number) 
+    {
+        if (turn < maxNumbers) 
+        {
+            Texts[turn].text = number.ToString(); 
+            turn++; 
+        }
+    }
+
+
+    
+}

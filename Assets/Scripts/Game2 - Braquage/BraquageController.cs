4bc24d6489adc287ce9a28206cef1e4c765b12f4
Zaksley <enzo8550@gmail.com>
2022-04-09 13:54:12 +0200

    adding time and nb lingots issues

--- Assets/Scripts/Game2 - Braquage/BraquageController.cs
+++ Assets/Scripts/Game2 - Braquage/BraquageController.cs
@@ -6,13 +6,48 @@ public class BraquageController : MonoBehaviour
 {
 
     private gameManager manager;
+    private int nbLingots = 0;
+    [SerializeField] int needLingots = 50;
+
+    [SerializeField] private float startTime = 60f; 
+    private float currentTime; 
+    private float displayTime;
+
     void Start()
     {
         // manager = GameObject.Find("dontDestroy").gameObject.GetComponent<dontDestroy>().save.GetComponent<gameManager>();
+
+        currentTime = startTime;
     }
 
-    void Update()
+void Update()
     {
-        
+        currentTime -= 1 * Time.deltaTime; 
+
+        displayTime = currentTime;
+        //countdown.text = displayTime.ToString("N1"); 
+
+        // Loose
+        if (currentTime <= 0)
+        {
+            currentTime = 0; 
+            //manager.defeat();
+        }
+    }
+
+    public void getLingot(int value)
+    {
+        nbLingots += value;
+        if (nbLingots >= needLingots)
+        {
+            //manager.win();
+        }
+
+    }
+
+    public void addTime(float timeValue)
+    {
+        currentTime += timeValue;
+
     }
 }

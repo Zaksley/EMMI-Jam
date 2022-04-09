4b9d1d21d286231433c5d7b4babccef2a1c80e19
alvina-dr <alvinadr24@gmail.com>
2022-04-09 15:01:08 +0200

    jeu race maj ajout délai mort + mort côté de la route + clignotement

--- Assets/Scripts/Game3 - Race/CarController.cs
+++ Assets/Scripts/Game3 - Race/CarController.cs
@@ -9,19 +9,15 @@ public class CarController : MonoBehaviour
     public GameObject leftLane;
     private GameObject mainCamera;
     public int speed;
-    private gameManager manager;
-    public int lives = 3;
+    public GameObject carSprite;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         transform.position = middleLane.transform.position;
-        manager = GameObject.Find("dontDestroy").gameObject.GetComponent<dontDestroy>().save.GetComponent<gameManager>();
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
-    // Update is called once per frame
     void Update()
     {
         //GO FORWARD
@@ -31,46 +27,31 @@ public class CarController : MonoBehaviour
         leftLane.transform.position = new Vector3(leftLane.transform.position.x, transform.position.y, leftLane.transform.position.z);
         mainCamera.transform.Translate(Vector3.up * Time.deltaTime * speed);
 
-
         //MOVEMENT
-        if (Input.GetKeyDown(KeyCode.RightArrow)) { //LE JOUEUR VA À DROITE
-            if (transform.position == rightLane.transform.position) { // si on est déjà à droite
-                //bruit de blocage ?
-            } else if (transform.position == middleLane.transform.position) { // si on est au milieu
-                transform.position = rightLane.transform.position;
-            } else if (transform.position == leftLane.transform.position) { //si on est à gauche
-                transform.position = middleLane.transform.position;
+        if(transform.position.x == carSprite.transform.position.x) {
+            if (Input.GetKeyDown(KeyCode.RightArrow)) { //LE JOUEUR VA À DROITE
+                if (transform.position == rightLane.transform.position) { // si on est déjà à droite
+                    //bruit de blocage + crash
+                    StartCoroutine(carSprite.GetComponent<CarSprite>().LoseLife());
+                } else if (transform.position == middleLane.transform.position) { // si on est au milieu
+                    transform.position = rightLane.transform.position;
+                } else if (transform.position == leftLane.transform.position) { //si on est à gauche
+                    transform.position = middleLane.transform.position;
+                }
+            } else if (Input.GetKeyDown(KeyCode.LeftArrow)) { // LE JOUEUR VA À GAUCHE
+                if (transform.position == rightLane.transform.position) { // si on est à droite
+                    transform.position = middleLane.transform.position;
+                } else if (transform.position == middleLane.transform.position) { // si on est au milieu
+                    transform.position = leftLane.transform.position;
+                } else if (transform.position == leftLane.transform.position) { //si on est déjà à gauche
+                    //bruit de blocage + crash
+                    StartCoroutine(carSprite.GetComponent<CarSprite>().LoseLife());
+                    //Debug.Log("Nombre de vie : " + carSprite.GetComponent<CarSprite>().lives);
+                }
             }
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) { // LE JOUEUR VA À GAUCHE
-            if (transform.position == rightLane.transform.position) { // si on est à droite
-                transform.position = middleLane.transform.position;
-            } else if (transform.position == middleLane.transform.position) { // si on est au milieu
-                transform.position = leftLane.transform.position;
-            } else if (transform.position == leftLane.transform.position) { //si on est déjà à gauche
-                //bruit de blocage ?
-            }
-        }
-
-        //FINAL GAME OVER
-        if (lives == 0) {
-            manager.defeat();
         }
 
     }
     
-    void OnTriggerEnter2D(Collider2D collision) {
-        //LOSE
-        if (collision.tag == "Obstacle") {
-            lives -= 1;
-            Debug.Log("Nombre de vie : " + lives);
 
-        }
-
-        //WIN
-        if (collision.tag == "Win") {
-            Debug.Log("bravooo");
-            manager.victory();
-
-        }
-    }
 }

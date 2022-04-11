cb842cc85e0ba51d9d1421f410a52f3efbb00c2a
alvina-dr <alvinadr24@gmail.com>
2022-04-11 22:09:41 +0200

    dialogues plus rapides

--- Assets/Scripts/Dialogues/DialogueManager.cs
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -57,7 +57,7 @@ public class DialogueManager : MonoBehaviour
         foreach (char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
-            yield return new WaitForSeconds(0.03f);
+            yield return new WaitForSeconds(0.006f);
         }
     }
     void EndDialogue()

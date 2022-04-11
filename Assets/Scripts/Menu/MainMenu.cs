1b0982ba8fdce03d8dd1ff0c44c7d6c0f4bd1c32
alvina-dr <alvinadr24@gmail.com>
2022-04-11 22:21:51 +0200

    fix des buttons du menu qui n'avaient pas de son grâce aux coroutines

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -10,14 +10,29 @@ public class MainMenu : MonoBehaviour
 
 
     public void StartGame() {
-        SceneManager.LoadScene("Beginning");
+        StartCoroutine(StartGameCoroutine());
     }
 
     public void QuitGame() {
-        Application.Quit();
+        StartCoroutine(QuitGameCoroutine());
     }
 
     public void ShowCredits() {
+        StartCoroutine(ShowCreditsCoroutine());
+    }
+
+    public IEnumerator StartGameCoroutine() {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("Beginning");
+    }
+
+    public IEnumerator QuitGameCoroutine() {
+        yield return new WaitForSeconds(0.5f);
+        Application.Quit();
+    }
+
+    public IEnumerator ShowCreditsCoroutine() {
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene("Crédits");
     }
 }
